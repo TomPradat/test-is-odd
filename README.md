ebc2cb808da11d0c20a2085bec1e23dee8e79652
Tom Pradat <tom.pradat@lemonlearning.fr>
2024-07-18 14:55:41 +0100

    new version

---
+++ README.md
@@ -0,0 +1,11 @@
+# Is odd
+
+This is a test package, do not use.
+
+Example :
+
+```typescript
+import isOdd from "@tomp/is-odd";
+
+isOdd(3); // true
+```
--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+/**
+ * Check if a number is odd
+ *
+ * @param candidate
+ * @returns boolean
+ */
 export default function isOdd(candidate: number): boolean {
   return candidate % 2 === 1;
 }
